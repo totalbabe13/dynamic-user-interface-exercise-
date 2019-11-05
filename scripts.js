b80335511079ebd3947257dbe97ad52e3c7cc2ba
leonardo Kaplan <kaplanleonardo@gmail.com>
2019-11-05 18:47:04 -0500

    set up html, working on creating css classes, and using js to animate

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-// alert("sha-nay-nay dont live here no more")
+let menuItem = document.getElementById('menu-element');
+let actaulMenu = document.getElementById("nav-bar-container");
+menuItem.onclick = dropMenu;
+
+function dropMenu(){
+  if(document.getElementsByClassName('visible').length === 0){
+    actaulMenu.className = "visible";
+  } else if (document.getElementsByClassName('visible').length !== 0) {
+     actaulMenu.classList.remove("visible");
+  }
+}
