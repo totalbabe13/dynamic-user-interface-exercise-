175a783ca19a33a5a35d4ef55b3c58c7edde8fd6
leonardo Kaplan <kaplanleonardo@gmail.com>
2019-11-10 16:46:49 -0500

    adding nav functionality to dots

--- image_carousel_demo/scripts.js
+++ image_carousel_demo/scripts.js
@@ -1,4 +1,16 @@
 function countSlides(){
   let numOfSlides = document.getElementsByClassName('slide')
-  return(numOfSlides.length);
+  return numOfSlides.length;
 };
+
+function makeNavDots(){
+  let numSlides    = countSlides();
+  let navContainer = document.getElementById('nav-dots');
+
+  for (var i = 0; i < numSlides; i++) {
+    let dot = document.createElement("div");
+    dot.className = "dot";
+    dot.setAttribute("data-index", `${i}`)
+    navContainer.appendChild(dot);
+    }
+  }
