6ec2de0022c75ace1f7f3dd3149822c5a4247c2b
leonardo Kaplan <kaplanleonardo@gmail.com>
2019-11-10 17:18:59 -0500

    dot navigation now functions

--- image_carousel_demo/scripts.js
+++ image_carousel_demo/scripts.js
@@ -10,7 +10,22 @@ function makeNavDots(){
   for (var i = 0; i < numSlides; i++) {
     let dot = document.createElement("div");
     dot.className = "dot";
-    dot.setAttribute("data-index", `${i}`)
+    dot.setAttribute("data-index", `${i}`);
+    dot.onclick = showImage;
     navContainer.appendChild(dot);
     }
   }
+function showImage() {
+  let dotIndex = this.dataset.index;
+  let currentlyVisible = document.getElementsByClassName("slide visible")[0]
+  let imageIndex = document.querySelectorAll(`[data-image='${dotIndex}']`)[0];
+
+  if(imageIndex.className === "slide visible"){
+    console.log("Already visible! dont change")
+  } else if (imageIndex.className !== "slide visible") {
+    currentlyVisible.className = "slide";
+    imageIndex.className = "slide visible";
+  }
+       // console.log(currentlyVisible);
+
+}
